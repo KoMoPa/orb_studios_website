5b2add525ac78ad75a4b92e29aa7a1e841fd0225
morgs999 <morgs99@gmail.com>
2026-04-16 17:28:59 -0400

    fixed link on front page

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -237,7 +237,7 @@ export default async function Home() {
               {
                 link: {
                   type: 'custom',
-                  url: '/recording',
+                  url: '/posts',
                   label: 'View Guides',
                   appearance: 'default',
                 },
